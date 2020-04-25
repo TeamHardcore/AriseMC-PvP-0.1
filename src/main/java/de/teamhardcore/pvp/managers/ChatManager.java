2a2ef83dda1e70442d05bed552221d20ee5d793f
TeamHardcore <tiipeexx13@gmail.com>
2020-04-26 01:30:19 +0200

    .

--- src/main/java/de/teamhardcore/pvp/managers/ChatManager.java
+++ src/main/java/de/teamhardcore/pvp/managers/ChatManager.java
@@ -8,13 +8,27 @@
 package de.teamhardcore.pvp.managers;
 
 import de.teamhardcore.pvp.Main;
+import de.teamhardcore.pvp.model.GlobalmuteTier;
 
 public class ChatManager {
 
     private final Main plugin;
 
-    public ChatManager(Main plugin){
+    private GlobalmuteTier globalmuteTier = GlobalmuteTier.NONE;
+
+    public ChatManager(Main plugin) {
         this.plugin = plugin;
     }
 
+    public GlobalmuteTier getGlobalmuteTier() {
+        return globalmuteTier;
+    }
+
+    public void setGlobalmuteTier(GlobalmuteTier globalmuteTier) {
+        this.globalmuteTier = globalmuteTier;
+    }
+
+    public Main getPlugin() {
+        return plugin;
+    }
 }
---
+++ src/main/java/de/teamhardcore/pvp/model/GlobalmuteTier.java
@@ -0,0 +1,18 @@
+/*
+ *
+ * @author TeamHardcore
+ *
+ * Copyright (c) 2020 by TeamHardcore to present. All rights reserved
+ */
+
+package de.teamhardcore.pvp.model;
+
+public enum GlobalmuteTier {
+
+    NONE,
+    WITHOUT_VERIFICATION,
+    ALL_PLAYERS,
+    COMPLETE;
+
+
+}
