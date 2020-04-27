dc2805871be38d06a5f73982e4401b18504d2eb0
TeamHardcore <Tiipeexx13@gmail.com>
2020-04-27 02:50:15 +0200

    Adding the crate system

--- src/main/java/de/teamhardcore/pvp/model/crates/crates/TestCrate.java
+++ src/main/java/de/teamhardcore/pvp/model/crates/crates/TestCrate.java
@@ -18,15 +18,15 @@ import java.util.List;
 public class TestCrate extends AbstractCrate {
 
     private final ArrayList<CrateItem> crateItems = new ArrayList<CrateItem>() {{
-        add(new CrateItem(new ItemBuilder(Material.BEDROCK).setDisplayName("§e§l12x Bedrock").setAmount(12).build(), "", 85, player -> {
+        add(new CrateItem(new ItemBuilder(Material.BEDROCK).setDisplayName("§e§l12x Bedrock").setAmount(12).build(), 85, player -> {
             player.getInventory().addItem(new ItemBuilder(Material.BEDROCK).setAmount(12).build());
             player.sendMessage(StringDefaults.PREFIX + "§eDu hast 12x Bedrock erhalten.");
         }));
-        add(new CrateItem(new ItemBuilder(Material.DOUBLE_PLANT).setDisplayName("§e§l250,000$").setAmount(25).build(), "", 10, player -> {
+        add(new CrateItem(new ItemBuilder(Material.DOUBLE_PLANT).setDisplayName("§e§l250,000$").setAmount(25).build(), 10, player -> {
             player.getInventory().addItem(new ItemBuilder(Material.BEDROCK).setAmount(12).build());
             player.sendMessage(StringDefaults.PREFIX + "§eDu hast §7250,000$ §eerhalten.");
         }));
-        add(new CrateItem(new ItemBuilder(Material.DOUBLE_PLANT).setDisplayName("§e§l500,000").setAmount(50).build(), "", 5, player -> {
+        add(new CrateItem(new ItemBuilder(Material.DOUBLE_PLANT).setDisplayName("§e§l500,000").setAmount(50).build(), 5, player -> {
             player.getInventory().addItem(new ItemBuilder(Material.BEDROCK).setAmount(12).build());
             player.sendMessage(StringDefaults.PREFIX + "§eDu hast §7500,000$ §eerhalten.");
         }));
